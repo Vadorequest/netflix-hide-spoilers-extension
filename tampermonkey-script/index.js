35e39e5cbe8e4622ef0c2a7e6edf616e117e144e
Vadorequest <ambroise.dhenain@gmail.com>
2020-02-13 14:05:40 +0100

    Hide spoilers in /browse page too

--- tampermonkey-script/index.js
+++ tampermonkey-script/index.js
@@ -8,20 +8,20 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  var css = '.evidence-overlay > p { display: none;}', // Hide spoilers
+    var css = '.evidence-overlay > p { display: none; } .overview .synopsis { display: none; }', // Hide spoilers
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 
-  head.appendChild(style);
+    head.appendChild(style);
 
-  style.type = 'text/css';
-  if (style.styleSheet) {
-    // This is required for IE8 and below.
-    style.styleSheet.cssText = css;
-  } else {
-    style.appendChild(document.createTextNode(css));
-  }
+    style.type = 'text/css';
+    if (style.styleSheet){
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
 })();
